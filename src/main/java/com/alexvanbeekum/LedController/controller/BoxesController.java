cd4cebef36c054b6742a60254be0188c3bfabc86
Alex <43249576+Ajv97@users.noreply.github.com>
2022-02-09 11:55:24 -0600

    Finished adding response entities for boxesController

--- src/main/java/com/alexvanbeekum/LedController/controller/BoxesController.java
+++ src/main/java/com/alexvanbeekum/LedController/controller/BoxesController.java
@@ -4,9 +4,13 @@ package com.alexvanbeekum.LedController.controller;
 import com.alexvanbeekum.LedController.entity.Boxes;
 import com.alexvanbeekum.LedController.payload.requests.ColorRequest;
 import com.alexvanbeekum.LedController.payload.requests.CreateBoxRequest;
+import com.alexvanbeekum.LedController.payload.responses.ListResponse;
+import com.alexvanbeekum.LedController.payload.responses.MessageResponse;
 import com.alexvanbeekum.LedController.repository.BoxesRepository;
 import com.alexvanbeekum.LedController.repository.SettingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,29 +26,26 @@ public class BoxesController {
 
     @CrossOrigin
     @PostMapping(path = "/box")
-    public @ResponseBody
-    String addNewBox(@RequestBody CreateBoxRequest box) {
+    public ResponseEntity<MessageResponse> addNewBox(@RequestBody CreateBoxRequest box) {
         Boxes b = new Boxes();
         b.setBox_id(box.getBoxId());
         b.setSet_id(box.getSetId());
         b.setLed_count(box.getLedCount());
         b.clearColor();
         bRepository.save(b);
-        return "saved";
+        return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse(HttpStatus.OK, "New Box created"));
     }
 
     @CrossOrigin
     @GetMapping(path="/boxes/{set_id}")
-    public @ResponseBody
-    Iterable<Boxes> getAllBoxes(@PathVariable int set_id) {
-        return bRepository.findAllSetId(set_id);
+    public ResponseEntity<ListResponse> getAllBoxes(@PathVariable int set_id) {
+        return ResponseEntity.ok(new ListResponse(bRepository.findAllSetId(set_id)));
     }
 
     @CrossOrigin
     @Transactional
     @PutMapping(path="/boxes/{set_id}/{box_id}")
-    public @ResponseBody
-    String updateBox(@PathVariable int set_id,
+    public ResponseEntity<MessageResponse> updateBox(@PathVariable int set_id,
                      @PathVariable int box_id,
                      @RequestBody ColorRequest color){
         if(color.isR() && color.isG() && color.isB()){
@@ -61,6 +62,6 @@ public class BoxesController {
             }
         }
         sRepository.lightsUpdated(set_id);
-        return "updated";
+        return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse(HttpStatus.OK, "New Box created"));
     }
 }
